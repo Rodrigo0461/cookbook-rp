f382ee0d8b93592c25168b8ee3cf9932c04d01e0
vagrant <vagrant@localhost.localdomain>
2019-12-10 15:03:58 +0000

    Initial workstation cookbook commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+	action:install
+end
+
+package 'ntp' do
+	action:install
+end
+
+package 'git' do
+	action:install
+end
+
+file '/etc/motd' do
+	content 'Propedidad Rp'
+	action :create
+   	owner 'root'
+	group 'root'
+end
